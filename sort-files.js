5c75430e182917a8530483f28effc0ee58ba7cfe
Hữu Ðà Trần <huuda.tran@gmail.com>
2018-12-31 11:05:31 -0500

    - Adding file-specific processing. - Ignoring some file.

--- sort-files.js
+++ sort-files.js
@@ -1,8 +1,13 @@
-const debug = require('./lib/debug')('sort-files');
+const debug = require('./debug')('sort-files');
 const constants = require('./lib/constants');
 const processFolder = require('./lib/process-folder');
 const Persistence = require('./lib/persistence');
 const args = require('./lib/args');
 
 const db = new Persistence(args.dest);
-processFolder(args.source, args.dest, args.limit);
+processFolder(args.source, args.dest, args.limit, {
+    any: !args.jpg && !args.cr2 && !args.mov,
+    cr2: args.cr2,
+    jpg: args.jpg,
+    mov: args.mov,
+});
